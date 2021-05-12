335ecb1039bfd6c8a4a03460f2cae0af54255b27
nakamura196 <na.kamura.1263@gmail.com>
2021-05-13 08:14:11 +0900

    add: library

--- assets/js/components/members.js
+++ assets/js/components/members.js
@@ -7,6 +7,7 @@ Vue.component('Members', {
       order: [],
       baseUrl: process.env.BASE_URL,
       dataUrl: process.env.DATA_URL,
+      peopleByPostionMap: {}
     }
   },
   async created() {
@@ -27,6 +28,34 @@ Vue.component('Members', {
     await axios.get(this.dataUrl + `/assets/json/faculty/${this.id}.json`).then(function (res) {
       self.order = res.data
     })
+
+    //日本語出ない場合は、職位で並び替え
+    if(this.lang !== "ja"){
+      const positionArray = ["教授", "准教授", "助教", "特任助教", "客員教授"]
+
+      const order = this.order
+      let flatOrder = []
+      for(const o of order){
+        flatOrder = flatOrder.concat(o.id.split("|"))
+      }
+
+      const people = this.people
+
+      const peopleByPostionMap = {}
+      for(const p of positionArray){
+        peopleByPostionMap[p] = []
+      }
+      
+      for(const id of flatOrder){
+        const p = people[id].position
+
+        if(!peopleByPostionMap[p].includes(id)){
+          peopleByPostionMap[p].push(id)
+        } 
+      }
+
+      this.peopleByPostionMap = peopleByPostionMap
+    }
   },
   methods: {
     $t(data) {
@@ -46,18 +75,16 @@ Vue.component('Members', {
       console.log({err})
     }
   },
-
   template: `<div>
     <h2 class="h03">{{$t("スタッフ・専攻テーマ")}}</h2>
-    <div v-for="(obj, key) in order" :key="key" class="mb-5">
-      <h3 class="h04">{{obj.room}}</h3>
-      
-      <template v-for="(index, key2) in obj.id.split('|')">
-        <template v-if="people[index]">
-          <div :key="key2" class="mb2 frame01">
-            <template v-if="lang === 'ja'">
+    <template v-if="lang === 'ja'">
+      <div v-for="(obj, key) in order" :key="key" class="mb-5">
+        <h3 class="h04">{{obj.room}}</h3>
+        <template v-for="(index, key2) in obj.id.split('|')">
+          <template v-if="people[index]">
+            <div :key="key2" class="mb2 frame01">
               <div>
-                {{$t(people[index].position)}} 
+                <span class="mr1">{{$t(people[index].position)}}</span>
                 <template v-if="people[index].url">
                   <a :href="people[index].url">
                     {{$t(people[index].surname)}} {{$t(people[index].forename)}}
@@ -87,41 +114,45 @@ Vue.component('Members', {
               <ul v-if="people[index].research">
                 <li v-for="(value, key3) in people[index].research.split('|')">{{value}}</li>
               </ul>
+            </div>
+          </template>
+          <template v-else>
+            {{elog(index)}}
+          </template>
+        </template>
+      </div>
+    </template>
+    <template v-else>
+      <template v-for="(arr, p) in peopleByPostionMap">
+        <div v-for="(index, key2) in arr" :key="p+key2" class="mb1 frame01">
+          <div>
+            <template v-if="people[index].url">
+              <a :href="people[index].url">
+                {{$t(people[index].surname_en)}} {{$t(people[index].forename_en)}}
+              </a>
             </template>
             <template v-else>
-              <div>
-                <template v-if="people[index].url">
-                  <a :href="people[index].url">
-                    {{$t(people[index].surname_en)}} {{$t(people[index].forename_en)}}
-                  </a>
-                </template>
-                <template v-else>
-                  {{$t(people[index].surname_en)}} {{$t(people[index].forename_en)}}
-                </template>, {{$t(people[index].position)}}
-              </div>
-              <template v-if="people[index].also !== ''">
-                <div v-for="(value, key3) in people[index].also.split('|')" :key="key3" v-if="value !== id">
-                  （Also 
-                  <a :href="localePath({name: value})">
-                    {{$t(value)}}
-                  </a>）
-                </div>
-              </template>
-              <template v-if="people[index].main !== id">
-                <div>
-                  （Also 
-                  <a :href="localePath({name: people[index].main})">
-                  {{$t(people[index].main)}}
-                  </a>）
-                </div>
-              </template>
-            </template>
+              {{$t(people[index].surname_en)}} {{$t(people[index].forename_en)}}
+            </template>, {{$t(people[index].position)}}
           </div>
-        </template>
-        <template v-else>
-          {{elog(index)}}
-        </template>
+          <template v-if="people[index].also !== ''">
+            <div v-for="(value, key3) in people[index].also.split('|')" :key="key3" v-if="value !== id">
+              （Also 
+              <a :href="localePath({name: value})">
+                {{$t(value)}}
+              </a>）
+            </div>
+          </template>
+          <template v-if="people[index].main !== id">
+            <div>
+              （Also 
+              <a :href="localePath({name: people[index].main})">
+              {{$t(people[index].main)}}
+              </a>）
+            </div>
+          </template>
+        </div>
       </template>
-    </div>
+    </template>
   </div>`,
 })
